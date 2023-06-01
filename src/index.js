2e36e91562e388777effb1e018bebc14109e5ebb
Alejandro <axelalejopc123@gmail.com>
2023-06-01 09:03:10 -0500

    [UP] Toggle functionality JS

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log("Hello");
+const menu = document.querySelector(".menu");
+const toggle = document.querySelector(".toggle");
+
+const toggleMenu = () => {
+  if (menu.classList.contains("responsive")) {
+    menu.classList.remove("responsive");
+    toggle.querySelector("a").innerHTML = "<i class='fas fa-bars'></i>";
+  } else {
+    menu.classList.add("responsive");
+    toggle.querySelector("a").innerHTML = "<i>X</i>";
+  }
+};
+
+toggle.addEventListener("click", toggleMenu);
+console.log("Working");
