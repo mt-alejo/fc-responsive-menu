341e478006b694c19ba6a54a7a756229192ae39e
Alejandro <axelalejopc123@gmail.com>
2023-06-01 09:49:33 -0500

    [UP] Toggle item functionality JS

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const menu = document.querySelector(".menu");
 const toggle = document.querySelector(".toggle");
 
-const toggleMenu = () => {
+function toggleMenu() {
   if (menu.classList.contains("responsive")) {
     menu.classList.remove("responsive");
     toggle.querySelector("a").innerHTML = "<i class='fas fa-bars'></i>";
@@ -9,7 +9,28 @@ const toggleMenu = () => {
     menu.classList.add("responsive");
     toggle.querySelector("a").innerHTML = "<i>X</i>";
   }
-};
+}
 
 toggle.addEventListener("click", toggleMenu);
-console.log("Working");
+
+const items = document.querySelectorAll(".item");
+
+function toggleItem() {
+  if (this.classList.contains("submenu-active")) {
+    this.classList.remove("submenu-active");
+  } else if (menu.querySelector(".submenu-active")) {
+    menu.querySelector(".submenu-active").classList.remove("submenu-active");
+    this.classList.add("submenu-active");
+  } else {
+    this.classList.add("submenu-active");
+  }
+  console.log(this.classList);
+}
+
+//LMAO CODE
+for (let item of items) {
+  //If you have a child with this ".className":
+  if (item.querySelector(".submenu")) {
+    item.addEventListener("click", toggleItem, false);
+  }
+}
